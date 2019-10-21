816988fec517eef63135cf6f0c849e9eb5d226d2
Andrew Murray <darbyguitars@gmail.com>
2019-10-21 01:11:04 -0500

    changing the layout of the commands

--- pop_install.sh
+++ pop_install.sh
@@ -1,41 +1,20 @@
 #!/bin/bash
 
+# update your machine first and foremost always then often
 sudo apt update
 sudo apt upgrade
 
-# ==> ==> ==> SHELL SETUP <== <== <== 
+# SHELL SETUP   
 sudo apt install zsh
 
 # prefered font
 sudo apt install fonts-firacode
 
-# install oh my zsh for terminal theme
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# ==> ==> ==> Development Tools <== <== <==
+# Development Tools
 sudo apt install -y mongodb 
 sudo apt install -y postgresql-11 
 sudo apt install -y guake 
 sudo apt install -y code
-
-# nvm install script from 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-
-# export the nvm path
-export NVM_DIR="$HOME/.nvm"
-# load nvm
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-# setup nvm bash completion
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-# set up npm to the latest verion
-nvm install node v10.16.0 --lts --latest-npm
-
-# ==> ==> ==> GLOBAL NPM PACKAGES <== <== <== 
-npm i -g create-react-app
-npm i -g express-generator
-npm i -g express-generator-api
-npm i -g nodemon
 
 # set up git so there are no warnings
 git config --global user.email darbyguitars@gmail.com
@@ -47,13 +26,37 @@ sudo apt install snapd
 # api testing suite
 snap install postman
 
-# preferred email client
-snap install mailspring
+# FUN THINGS ALL THESE BELOW CAN BE CHAINED TOGETHER TO PRINT A RAINBOW TURTLE IN YOUR TERMINAL THAT GIVES YOU LINUX TIPS
 
-# ==> ==> ==> FUN THINGS <== <== <==
+# rainbow output in terminal
 sudo apt install lolcat
+
+# pictures of various animals talking
 sudo apt install cowsay
+
+# random quotes from a list
 sudo apt install fortune
 
-source ~/.zshrc
-reboot
+# this sets up oh my zsh this will restart your shell so onthing will run after it
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# nvm install script from 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+# export the nvm path
+export NVM_DIR="$HOME/.nvm"
+# load nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# setup nvm bash completion
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# down load all lts versions of node
+nvm install --lts
+
+# GLOBAL NPM PACKAGES 
+npm i -g create-react-app
+npm i -g express-generator
+npm i -g express-generator-api
+npm i -g nodemon
+npm i -g gitignore
+
